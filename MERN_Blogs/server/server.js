597b2475876ac42beb2c31be4a41a24342180bd0
Yasvanth-2005 <yasvanthhanumantu1@gmail.com>
2023-06-27 22:51:31 +0530

    set up blog folder

---
+++ MERN_Blogs/server/server.js
@@ -0,0 +1,11 @@
+const express = use('express');
+const mongoose = use('mongoose');
+const app = express()
+
+app.post('/register',(req,res) => {
+
+})
+
+app.listen(4000,() => {
+    console.log("Server running on port 4000");
+})
